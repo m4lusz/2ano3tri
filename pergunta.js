1c3d9de1a98d0c20c07abb98bb1f25a0cb1cd194
malu <132919611+m4lusz@users.noreply.github.com>
2024-09-09 12:37:59 -0300

    im

---
+++ pergunta.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Inglaterra?',
+    'A capital da Inglaterra é Londres'
+)
+
+criaCartao(
+    'Língua Inglesa',
+    'Como se fala "cadeira" em inglês?',
+    'chair'
+)
+
+criaCartao(
+    'Artes',
+    'Azul e amarelo juntos viram...',
+    'verde!'
+)
